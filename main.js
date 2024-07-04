34ba1ca4b4c9a49d09469203ec4fa3ca7e4180b1
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:30:07 -0700

    Add files via upload
    Q11, 45 Projects

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+Q11. Names: Store the names of a few of your friends in a array called names. Print each person’s
+ name by accessing each element in the list, one at a time.
+
+*/
+var friendsArray = ["Ayaan", "Sana", "Sajida", "Atif", "Angelis Aura"];
+for (var i = 0; i < friendsArray.length; i++) {
+    console.log(friendsArray[i]);
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+/*
+Q11. Names: Store the names of a few of your friends in a array called names. Print each person’s
+ name by accessing each element in the list, one at a time.
+*/
+
+let friendsArray = ["Ayaan","Sana","Sajida","Atif","Angelis Aura"];
+for (let i=0; i<friendsArray.length; i++){
+    console.log(friendsArray[i]);
+}
